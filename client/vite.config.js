697fd3531fc6cbacf94cdf61709b2d6a6c6288bb
Jimmy Dickinson <jamesfdickinson@gmail.com>
2026-09-11 22:35:58 -0700

    mine updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./",
   server: {
-    host: "0.0.0.0"
+    host: "0.0.0.0",
+    port: 4173,
+    strictPort: true
   },
   build: {
     outDir: "dist",
